4750d09455d71f9992a9bcc07b4afcec84576a55
Azwad-Ishraq <azwadishraq@.com>
2022-04-25 20:20:48 +0600

    third commit

--- app.js
+++ app.js
@@ -19,3 +19,34 @@ menus.forEach(menu=> {
 
     })
 })
+
+
+
+// get HR value from input
+const onChange = (e) => {
+    console.log(e.target.value);
+}
+const hrInput = document.getElementById("hr-textField");
+
+hrInput.addEventListener("keypress", (e) => {
+
+    if(e.key === "Enter"){
+
+        const value = parseInt(e.target.value);
+        
+        if(isNaN(value)){
+            console.log("Please enter e number");
+        }
+        
+        else{
+            console.log(value);
+
+            if(value > 42.5 || value < 2.5){
+                
+            }
+        }
+
+    }
+
+
+})
